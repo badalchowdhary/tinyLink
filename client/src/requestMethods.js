fba2a01cf9b74fbf4a56d0af70e9759f3770c38a
badalchowdhary <76513149+badalchowdhary@users.noreply.github.com>
2024-06-20 11:53:49 +0530

    Update requestMethods.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = "http://localhost:8000";
 
-export { BASE_URL };
+// export { BASE_URL };
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
 });
