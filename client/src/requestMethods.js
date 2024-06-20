83bccc24e1bc2b7f861147e848518ecb67bdc2f8
badalchowdhary <76513149+badalchowdhary@users.noreply.github.com>
2024-06-20 12:12:13 +0530

    Update requestMethods.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://tiny-link-server.vercel.app";
 
 // export { BASE_URL };
 export const publicRequest = axios.create({
